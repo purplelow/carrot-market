1ee9cbdaa0f374cfa18abf9ffeac0a21188c3a50
JSCreation <purplelow1@gmail.com>
2022-04-11 01:19:38 +0900

    Feat: "enter화면 추가"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,jsx,tx,tsx}",
-    ".components/**/*.{js.jsx.ts.tsx}",
-  ],
+  content: ["./pages/**/*.{js,jsx,tx,tsx}", ".components/**/*.{js.jsx.ts.tsx}"],
   theme: {
     extend: {},
   },
+  darkMode: "class",
   plugins: [],
-}
+};
