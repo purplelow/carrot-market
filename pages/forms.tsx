ef6012409de586cd251a0d817910e59aae3538fa
JSCreation <purplelow1@gmail.com>
2022-05-02 00:05:19 +0900

    Feat:Prisma 수정

---
+++ pages/forms.tsx
@@ -0,0 +1,9 @@
+export default function Forms() {
+  return (
+    <form>
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Username" />
+    </form>
+  );
+}
